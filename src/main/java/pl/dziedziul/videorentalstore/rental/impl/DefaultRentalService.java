2f17064f93c0f9acdfa9d5722f270bf67de39b67
Łukasz Dziedziul <l.dziedziul@gmail.com>
2019-03-21 11:28:55 +0100

    implement renting

---
+++ src/main/java/pl/dziedziul/videorentalstore/rental/impl/DefaultRentalService.java
@@ -0,0 +1,46 @@
+package pl.dziedziul.videorentalstore.rental.impl;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import pl.dziedziul.videorentalstore.customer.CustomerService;
+import pl.dziedziul.videorentalstore.rental.RentFilmsCommand;
+import pl.dziedziul.videorentalstore.rental.RentalDto;
+import pl.dziedziul.videorentalstore.rental.RentalService;
+
+@Slf4j
+@Service
+class DefaultRentalService implements RentalService {
+
+    private final CustomerService customerService;
+    private final RentalRepository rentalRepository;
+    private final RentalEntityCreator rentalEntityCreator;
+
+    DefaultRentalService(
+        final @NonNull CustomerService customerService,
+        final @NonNull RentalRepository rentalRepository,
+        final @NonNull RentalEntityCreator rentalEntityCreator) {
+        this.customerService = customerService;
+        this.rentalRepository = rentalRepository;
+        this.rentalEntityCreator = rentalEntityCreator;
+    }
+
+    @Override
+    @Transactional
+    public RentalDto rentFilms(final RentFilmsCommand command) {
+        log.info("Renting films: " + command);
+        validateCustomerExistence(command.getCustomerId());
+        RentalEntity rentalEntity = rentalRepository.save(rentalEntityCreator.create(command));
+        return new RentalDto(rentalEntity.getId(), rentalEntity.getTotalPrice());
+    }
+
+    private void validateCustomerExistence(final UUID customerId) {
+        customerService.getCustomer(customerId);
+    }
+
+
+}
---
+++ src/test/groovy/pl/dziedziul/videorentalstore/rental/impl/DefaultRentalServiceTest.groovy
@@ -0,0 +1,39 @@
+package pl.dziedziul.videorentalstore.rental.impl
+
+import pl.dziedziul.videorentalstore.customer.CustomerNotFoundException
+import pl.dziedziul.videorentalstore.customer.CustomerService
+import pl.dziedziul.videorentalstore.rental.RentFilmsCommand
+import pl.dziedziul.videorentalstore.rental.RentalDto
+import pl.dziedziul.videorentalstore.test.TestData
+import spock.lang.Specification
+
+class DefaultRentalServiceTest extends Specification {
+
+    def "should rent films"() {
+        given:
+        def rentalRepository = Mock(RentalRepository)
+        def rentalCreator = Mock(RentalEntityCreator)
+        def tested = new DefaultRentalService(Mock(CustomerService), rentalRepository, rentalCreator)
+        1 * rentalCreator.create(_) >> new RentalEntity(TestData.SOME_CUSTOMER_ID, TestData.SOME_INSTANT)
+                .withRentedFilm(new RentedFilmEntity(TestData.SOME_FILM_ID, TestData.SOME_PRICE, TestData.SOME_NUMBER_OF_DAYS))
+        1 * rentalRepository.save(_) >> { args -> args[0] }
+        when:
+        RentalDto rental = tested.rentFilms(new RentFilmsCommand(TestData.SOME_CUSTOMER_ID, Set.of()))
+        then:
+        rental.id
+        rental.price == TestData.SOME_PRICE
+    }
+
+    def "should fail when customer not found"() {
+        given:
+        def customerService = Stub(CustomerService) {
+            getCustomer(_) >> { args -> throw new CustomerNotFoundException(args[0]) }
+        }
+        def tested = new DefaultRentalService(customerService, Mock(RentalRepository), Mock(RentalEntityCreator))
+        when:
+        tested.rentFilms(new RentFilmsCommand(TestData.SOME_CUSTOMER_ID, Set.of()))
+        then:
+        thrown(CustomerNotFoundException)
+    }
+
+}
